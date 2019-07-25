0889232c9311bf69bf2e36ef84653f2913d60a32
s.prokhorov <s.prokhorov@geschenkidee.de>
2019-07-25 17:31:40 +0200

    add shutdown timeout test and comments

--- launcher.go
+++ launcher.go
@@ -11,11 +11,18 @@ import (
 	"time"
 )
 
+// ErrServersListEmpty returned by Run method if there are no servers added.
+var (
+	ErrServersListEmpty = errors.New("servers list is empty")
+)
+
+// Server describes required server methods.
 type Server interface {
 	Serve() error
 	Shutdown(ctx context.Context) error
 }
 
+// Launcher manages Servers from internal stored list.
 type Launcher struct {
 	ch              chan bool
 	waitGroup       *sync.WaitGroup
@@ -23,54 +30,36 @@ type Launcher struct {
 	shutdownTimeout time.Duration
 }
 
+// New returns a new Launcher.
 func New() *Launcher {
 	return &Launcher{
 		ch:              make(chan bool),
 		waitGroup:       &sync.WaitGroup{},
 		servers:         map[string]Server{},
-		shutdownTimeout: 1,
+		shutdownTimeout: 60,
 	}
 }
 
+// Add adds new server to the internal servers list.
 func (srv *Launcher) Add(name string, server Server) {
 	srv.servers[name] = server
 	srv.waitGroup.Add(1)
 }
 
-func (srv *Launcher) stopServers() {
-	ctx, cancel := context.WithTimeout(
-		context.Background(),
-		srv.shutdownTimeout*time.Second,
-	)
-	defer cancel()
-
-	for name, server := range srv.servers {
-		go func(n string, s Server) {
-			log.Printf("Trying to stop server %s", n)
-			if err := s.Shutdown(ctx); err != nil {
-				log.Printf("Failed to stop server %s, %+v", n, err)
-			}
-		}(name, server)
-	}
+// SetShutdownTimeout change shutdown timeout. Default is 60 seconds.
+func (srv *Launcher) SetShutdownTimeout(duration time.Duration) {
+	srv.shutdownTimeout = duration
 }
 
-func (srv *Launcher) startServers(wg *sync.WaitGroup) {
-	for name, server := range srv.servers {
-		go func(n string, s Server) {
-			log.Printf("Start server %s", n)
-			if err := s.Serve(); err != nil {
-				log.Printf("Server %s has been stoped or failed to start, %+v", n, err)
-			}
-			log.Println("Server terminated successfully")
-			wg.Done()
-		}(name, server)
-	}
-}
-
+// Run starts all servers from internal list. It will return ErrServersListEmpty
+// if servers list is empty.
+//
+// Run method listens syscalls(SIGINT, SIGTERM, SIGQUIT) and calles Server.Shutdown
+// method.
 func (srv *Launcher) Run() error {
 	// Check setup
 	if len(srv.servers) <= 0 {
-		return errors.New("servers list is empty")
+		return ErrServersListEmpty
 	}
 
 	// Subscribe to signals
@@ -96,19 +85,45 @@ func (srv *Launcher) Run() error {
 	wg := &sync.WaitGroup{}
 	wg.Add(len(srv.servers))
 
-	// log.Println(syscall.Getpid())
-
 	// Start servers
 	srv.startServers(wg)
 	wg.Wait()
 	return nil
 }
 
-// func main() {
-// 	srv := New()
-// 	hs1 := NewHS(":8080")
-// 	srv.Add("http1", hs1)
-// 	hs2 := NewHS(":8080")
-// 	srv.Add("http2", hs2)
-// 	srv.Run()
-// }
+// Stop terminates servers. This method needed for manual servers stop.
+func (srv *Launcher) Stop() {
+	srv.stopServers()
+}
+
+// stopServers loops through the servers list and stops all of those.
+func (srv *Launcher) stopServers() {
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		srv.shutdownTimeout*time.Second,
+	)
+	defer cancel()
+
+	for name, server := range srv.servers {
+		go func(n string, s Server) {
+			log.Printf("Trying to stop server %s", n)
+			if err := s.Shutdown(ctx); err != nil {
+				log.Printf("Failed to stop server %s, %+v", n, err)
+			}
+		}(name, server)
+	}
+}
+
+// startServers loops through the servers list and starts all of those.
+func (srv *Launcher) startServers(wg *sync.WaitGroup) {
+	for name, server := range srv.servers {
+		go func(n string, s Server) {
+			log.Printf("Start server %s", n)
+			if err := s.Serve(); err != nil {
+				log.Printf("Server %s has been stoped or failed to start, %+v", n, err)
+			}
+			log.Println("Server terminated successfully")
+			wg.Done()
+		}(name, server)
+	}
+}
--- launcher_test.go
+++ launcher_test.go
@@ -2,46 +2,80 @@ package launcher
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"syscall"
 	"testing"
 	"time"
 )
 
-type HTTPServer struct {
+type httpServer struct {
 	srv  *http.Server
 	addr string
 }
 
-func NewHS(port string) *HTTPServer {
-	return &HTTPServer{addr: port}
+func newHS(port string) Server {
+	return &httpServer{addr: port}
 }
 
-func (hs *HTTPServer) Serve() error {
+func (hs *httpServer) Serve() error {
 	hs.srv = &http.Server{}
 	hs.srv.Addr = hs.addr
-
-	// hs.srv.Ha
-	// http.Handle("/foo", fooHandler)
-
-	// http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	// })
-
 	return hs.srv.ListenAndServe()
 }
 
-func (hs *HTTPServer) Shutdown(ctx context.Context) error {
+func (hs *httpServer) Shutdown(ctx context.Context) error {
 	return hs.srv.Shutdown(context.Background())
 }
 
 func TestRun(t *testing.T) {
 	l := New()
-	hs := NewHS(":8080")
+	hs := newHS(":8080")
 	l.Add("http1", hs)
 	go func() {
 		time.Sleep(1 * time.Second)
 		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
 	}()
+	l.Run()
+}
+
+type customServer struct {
+	shutdown chan struct{}
+	ctx      context.Context
+}
+
+func (cs *customServer) Serve() error {
+	<-cs.shutdown
+	log.Println("Custom server is shutdown")
+	return nil
+}
+
+func (cs *customServer) Shutdown(ctx context.Context) error {
+	for {
+		select {
+		case <-ctx.Done():
+			close(cs.shutdown)
+			return ctx.Err()
+		}
+	}
+}
+
+func newCS() Server {
+	return &customServer{shutdown: make(chan struct{})}
+}
+
+func TestShutdownTimeout(t *testing.T) {
+	cs := newCS()
+
+	l := New()
+	l.SetShutdownTimeout(1)
+	l.Add("custom1", cs)
+
+	go func() {
+		log.Println("Stop server in 2 seconds")
+		time.Sleep(2 * time.Second)
+		l.Stop()
+	}()
+
 	l.Run()
 }
